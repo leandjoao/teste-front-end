195c699ff6887fc04324cdb56c08bd5fc828aac3
leandjoao <leand.joao@outlook.com.br>
2020-08-29 03:57:19 -0300

    end

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 import "./style.css";
+let lista = document.querySelector('.list');
+let buttons = [];
 
-let app = null;
-let list = null;
-let dropdown = null;
+const fetchList = () => {
+  return fetch('https://swapi.dev/api/people/', {
+    method: "GET",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+  })
+  .then(response => response.json())
+  .then(responseData => {
+    let list = responseData.results;
+    let lista = document.createElement("ul");
+    list.forEach(personagem => {
+      let {
+        name: nome,
+        eye_color: color,
+        ...rest
+      } = personagem
 
-window.addEventListener("ready", () => {
-  app = document.querySelector("#app");
-  list = app.querySelector(".list");
-});
-
-const fetchList = async () => {
-  let response = await fetch('https://swapi.dev/api/people/');
-  if (response.ok) {
-    let json = await response.json();
-    json.results.forEach(item => {
-      let personagem = {
-        name,
-        eye_color
-      } = item;
-
-      console.log(personagem)
+      lista.appendChild(criarLista(nome, color));
+      document.querySelector("#app").appendChild(lista);
     })
-  } else {
-    alert("HTTP-Error: " + response.status);
-  }
+  })
+  .then(() => setButtons())
+  .catch(error => console.warn(error));
 }
 
 fetchList();
+
+const criarLista = (nome, cor) => {
+  let item = document.createElement('li');
+  item.setAttribute("class", nome.split(' ').join(''));
+  item.innerHTML = `
+  <p style="color:${cor}">${nome}</p>
+  <button id=${nome.split(' ').join('')}>remover</button>
+  `
+  return item;
+}
+
+const setButtons = () => {
+  buttons = document.querySelectorAll("button");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", item => {
+      let selected = document.querySelector('.' + item.target.id);
+      selected.remove();
+    })
+  })
+}
